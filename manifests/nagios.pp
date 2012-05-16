73c111284c9ab9095d805b123b6403ef69b463c4
Yoann Laissus <yoann.laissus@gmail.com>
2012-05-16 15:30:35 +0200

    Fix indentation

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -1,12 +1,12 @@
  
 define ferm::nagios($nagios_host=false, $table="filter", $chain="INPUT", $rules, $prio="00") {
-        file {
-            "/etc/ferm/rules.d/${prio}_nagios":
-                ensure  => present,
-                owner   => root,
-                group   => root,
-                mode    => 0400,
-                content => template("ferm/nagios.erb"),
-                notify  => Service["ferm"];
-        }
-    }
+	file {
+		"/etc/ferm/rules.d/${prio}_nagios":
+			ensure  => present,
+			owner   => root,
+			group   => root,
+			mode    => 0400,
+			content => template("ferm/nagios.erb"),
+			notify  => Service["ferm"];
+	}
+}
