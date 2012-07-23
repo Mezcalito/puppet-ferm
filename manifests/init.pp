d27b725dbda370f462cce8bd62eac2c2d1dd8363
Yoann Laissus <yoann.laissus@gmail.com>
2012-07-23 12:22:28 +0200

    Move the ferm::rule type in its own file. Move all ferm::rule type definition in the rules subdir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,4 @@
 class ferm {
-	define rule($host = false, $table="filter", $chain="INPUT", $rules, $description="", $prio="00", $notarule=false) {
-		file { "/etc/ferm/rules.d/${prio}_${name}":
-			ensure  => present,
-			owner   => root,
-			group   => root,
-			mode    => 0400,
-			content => template("ferm/ferm-rule.erb"),
-			notify  => Exec["refresh_ferm"];
-		}
-	}
 
 	package {
 		ferm: ensure => installed;
