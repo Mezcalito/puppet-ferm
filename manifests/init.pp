e86559eda0ebb7f72f36ac0e01d62a9da55b1e52
Sophie Mboup <sophie@mezcalito.fr>
2018-05-25 09:42:36 +0200

    correct error indentation, double quoted and two space soft tabs in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,50 +3,43 @@ class ferm (
   $cache = true,
   $fast = true,
 ){
-
-	package { 'ferm':
+  package { 'ferm':
     ensure => installed,
-	}
-
-  file { "/etc/ferm/rules.d":
-    ensure => directory,
+  }
+  file { '/etc/ferm/rules.d':
+    ensure  => directory,
     purge   => true,
     owner   => root,
     group   => root,
     force   => true,
     recurse => true,
-    notify  => Exec["refresh_ferm"],
-    require => Package["ferm"],
+    notify  => Exec['refresh_ferm'],
+    require => Package['ferm'],
   }
-
-	file { "/etc/ferm/conf.d":
+  file { '/etc/ferm/conf.d':
     ensure  => directory,
     owner   => root,
     group   => root,
-    require => Package["ferm"],
+    require => Package['ferm'],
   }
-
-	file {	"/etc/default/ferm":
-    content  => template("ferm/ferm.default.erb"),
+  file { '/etc/default/ferm':
+    content => template('ferm/ferm.default.erb'),
     owner   => root,
     group   => root,
-    require => Package["ferm"],
-    notify  => Exec["refresh_ferm"],
+    require => Package['ferm'],
+    notify  => Exec['refresh_ferm'],
   }
-
-	file { "/etc/ferm/ferm.conf":
-    content  => template("ferm/ferm.conf.erb"),
+  file { '/etc/ferm/ferm.conf':
+    content => template('ferm/ferm.conf.erb'),
     owner   => root,
     group   => root,
     mode    => '0400',
-    require => Package["ferm"],
-    notify  => Exec["refresh_ferm"],
-	}
-
-  exec { "refresh_ferm":
-    command => "/etc/init.d/ferm restart",
-    require  => Package["ferm"],
+    require => Package['ferm'],
+    notify  => Exec['refresh_ferm'],
+  }
+  exec { 'refresh_ferm':
+    command     => '/etc/init.d/ferm restart',
+    require     => Package['ferm'],
     refreshonly => true
   }
-  
 }
