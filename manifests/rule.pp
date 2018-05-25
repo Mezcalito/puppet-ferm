4ac6b4d4b13bfae3c2f4d40e8b0130bb2e7c85e9
Sophie Mboup <sophie@mezcalito.fr>
2018-05-25 09:46:14 +0200

    correct error double quoted, two space soft tabs, tab character and  put required parameter before optional parameter

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,21 +1,20 @@
 define ferm::rule(
-	$host = undef,
-	$interface = undef,
-	$daddr = undef,
-	$table = "filter",
-	$chain = "INPUT",
-	$rules,
-	$description = "",
-	$prio = "00",
-	$notarule = false
+  $rules,
+  $host = undef,
+  $interface = undef,
+  $daddr = undef,
+  $table = 'filter',
+  $chain = 'INPUT',
+  $description = '',
+  $prio = '00',
+  $notarule = false
 ) {
-
   file { "/etc/ferm/rules.d/${prio}_${name}":
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0400',
-    content => template("ferm/ferm-rule.erb"),
-    notify  => Exec["refresh_ferm"],
+    content => template('ferm/ferm-rule.erb'),
+    notify  => Exec['refresh_ferm'],
   }
 }
